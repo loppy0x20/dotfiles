410a5ab88da0e95b576d95f9b4ccca04539f5ebe
Shane Young <shane.young@shaneyo-ltmbhzv.internal.salesforce.com>
2022-10-18 08:48:02 -0500

    added blackarch

--- install_arch.sh
+++ install_arch.sh
@@ -9,6 +9,7 @@ if [ -f /etc/os-release ]; then
         # oh-my-zsh
         sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
         git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime && sh ~/.vim_runtime/install_awesome_vimrc.sh
+        cd /tmp/ && curl -O https://blackarch.org/strap.sh && chmod +x strap.sh && ./strap.sh && rm -rf /tmp/*
     fi
 fi
 
