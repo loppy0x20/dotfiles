43e45d8700290819548befd4e86dc00843446a9e
Shane Young <shane.young@shaneyo-ltmbhzv.internal.salesforce.com>
2022-10-18 11:21:36 -0500

    updates

--- install_arch.sh
+++ install_arch.sh
@@ -51,7 +51,7 @@ if [ -f /etc/os-release ]; then
     fi
 fi
 
-cp $(PWD)/.zprofile $HOME/.zprofile
-cp $(PWD)/.zshrc $HOME/.zshrc
+curl -O $HOME/.zprofile https://raw.githubusercontent.com/x90skysn3k/dotfiles/main/.zprofile
+curl -O $HOME/.zshrc https://raw.githubusercontent.com/x90skysn3k/dotfiles/main/.zshrc
 
 chsh -s $(which zsh)
