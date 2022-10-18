29636d94745e04058e3cd9065e7b8b0ac783c0c5
Shane Young <shane.young@shaneyo-ltmbhzv.internal.salesforce.com>
2022-10-18 11:00:20 -0500

    tools

--- install_arch.sh
+++ install_arch.sh
@@ -37,7 +37,8 @@ if [ -f /etc/os-release ]; then
         whois \
         xclip \
         go \
-        zip
+        zip \
+        zsh
 
         # oh-my-zsh
         sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
