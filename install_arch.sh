4c124ebf18b42b8c749ca4a1cf5581d68272a434
Shane Young <shane.young@shaneyo-ltmbhzv.internal.salesforce.com>
2022-10-18 10:47:27 -0500

    tools

--- install_arch.sh
+++ install_arch.sh
@@ -4,7 +4,42 @@ if [ -f /etc/os-release ]; then
     . /etc/os-release
 
     if [ "$ID" == "arch" ]; then
-        pacman -Syu --noconfirm
+        pacman -Syy --noconfirm && \
+        pacman -S --noconfirm --disable-download-timeout --verbose \
+        aws-cli \
+        zsh \
+        base-devel \
+        ca-certificates \
+        cifs-utils \
+        curl \
+        dnsutils \
+        file \
+        freerdp \
+        git \
+        gnupg \
+        grep \
+        htop \
+        iputils \
+        jq \
+        openldap \
+        net-tools \
+        gnu-netcat \
+        nfs-utils \
+        openssl \
+        proxychains \
+        python3 \
+        python-pip \
+        smbclient \
+        openssh \
+        tmux \
+        tree \
+        tzdata \
+        unzip \
+        vim \
+        whois \
+        xclip \
+        go \
+        zip
 
         # oh-my-zsh
         sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
