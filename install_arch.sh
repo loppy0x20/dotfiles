6e7b66f47a5224b388eb0cd8919a12352de0e529
Shane Young <shane.young@shaneyo-ltmbhzv.internal.salesforce.com>
2022-10-18 08:40:32 -0500

    add arch

---
+++ install_arch.sh
@@ -0,0 +1,24 @@
+#/bin/bash
+
+if [ -f /etc/os-release ]; then
+    . /etc/os-release
+
+    if [ "$ID" == "arch" ]; then
+        pacman -Syu --noconfirm
+
+        # oh-my-zsh
+        sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+        git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime && sh ~/.vim_runtime/install_awesome_vimrc.sh
+    fi
+fi
+
+cp ./.zprofile $HOME/.zprofile
+cp ./.zshrc $HOME/.zshrc
+cp ./.tmux.conf $HOME/.tmux.conf
+
+sudo cp ./.zprofile /root/.zprofile
+sudo cp ./.zshrc    /root/.zshrc
+sudo cp ./.tmux.conf /root/.tmux.conf
+
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+~/.tmux/plugins/tpm/scripts/install_plugins.sh
